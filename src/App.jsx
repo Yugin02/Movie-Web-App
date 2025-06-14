adeac21a1764f74291d3a5659a2bd69fd8e8587f
Eugene Gerna <eugenegerna02@gmail.com>
2025-06-14 00:43:32 -1200

    fix namin

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import {useEffect, useState} from 'react'
-import Search from './components/search'
+import Search from './components/SearchBar';
 import Spinner from './components/Spinner';
 import Card from './components/Card';
 import { useDebounce } from 'react-use';
